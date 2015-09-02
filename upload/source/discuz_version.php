5af2432808722f1653b561a7076827acdb6e3a61
jaideejung007 <devnull@localhost>
2015-09-02 09:21:44 +0700

    IMPROVE: GeoIPLoc code & data last updated: Mon Aug 31 0:02:04 PDT 2015

--- upload/source/discuz_version.php
+++ upload/source/discuz_version.php
@@ -15,7 +15,7 @@ if(!defined('DISCUZ_VERSION')) {
 	define('DISCUZ_VERSION', 'X3.2');
 	define('DISCUZ_RELEASE', '20150609');
 	define('DISCUZ_FIXBUG', '32000000');
-	define('DISCUZ_TH_REVISION', '38');
+	define('DISCUZ_TH_REVISION', '39');
 }
 
 ?>
