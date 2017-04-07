b43e3d338a9d8fe073a0efa01435628ae8260056
jaideejung007 <devnull@localhost>
2017-04-07 12:11:40 +0700

    Release: Discuz! X3.3 R20170401 FIX: Thai translation icon post, reply, tweet and create a group files. FIX: wysiwyg.css, add a slash after IMGDIR FIX: magic XSS vulnerability. FIX: common_extra.js FIX: home/magic XSS vulnerability. FIX: a bug in delete a favorite post. FIX: for a thread type post button.

--- upload/source/discuz_version.php
+++ upload/source/discuz_version.php
@@ -13,9 +13,9 @@ if(!defined('IN_DISCUZ')) {
 
 if(!defined('DISCUZ_VERSION')) {
 	define('DISCUZ_VERSION', 'X3.3');
-	define('DISCUZ_RELEASE', '20170301');
+	define('DISCUZ_RELEASE', '20170401');
 	define('DISCUZ_FIXBUG', '33000000');
-	define('DISCUZ_TH_REVISION', '53'); /*jaideejung007*/
+	define('DISCUZ_TH_REVISION', '54'); /*jaideejung007*/
 }
 
 ?>
