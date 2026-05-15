0ca45f6d59927838257b319d5cf388c9ad7b9467
jaideejung007 <jaideejung007@gmail.com>
2026-05-15 21:56:10 +0700

    🆙 อัปเดต (3d5ae26): อัปเดตเลขเวอร์ชันเป็น 20260510
    - 🔖 [Version] อัปเดตหมายเลขเวอร์ชันของระบบเป็น 20260510

--- upload/source/discuz_version.php
+++ upload/source/discuz_version.php
@@ -14,6 +14,6 @@ if(!defined('DISCUZ_VERSION')) {
 	define('DISCUZ_VERSION_NAME', 'Discuz! &#xE23;&#xE38;&#xE48;&#xE19;&#xE42;&#xE2D;&#xE40;&#xE1E;&#xE19;&#xE0B;&#xE2D;&#xE23;&#xE4C;&#xE2A;');
 	define('DISCUZ_VERSION', 'X5.0');
 	define('DISCUZ_SUBVERSION', '.0.Open');
-	define('DISCUZ_RELEASE', '20260501');
+	define('DISCUZ_RELEASE', '20260510');
 }
 
